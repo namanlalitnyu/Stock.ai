61ad4964e55511ac49f7749882742f93756e9cd5
Naman Lalit <namanlalit@Namans-MacBook-Air.local>
2024-04-29 22:49:11 -0400

    Updated Dockerfiles for backend and frontend; Updated Docker-compose.yml

--- backend/app.py
+++ backend/app.py
@@ -145,4 +145,4 @@ def create_recommendation_prompt(question, documents):
   return prompt + documentsPrompt
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(port=3000)
